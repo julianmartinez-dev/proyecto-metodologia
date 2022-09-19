a4879f3afaf501065f3699a72be0bf93003326f2
Fabrizio Peirano <fabripeirano@gmail.com>
2022-09-19 18:55:36 -0300

    Commands Create and FindByIdentCard

---
+++ server/src/application/commands/create.passenger.command.ts
@@ -0,0 +1,27 @@
+export class CreateUserCommand {
+    private readonly fullname: string;
+    private readonly email: string;
+    private readonly identityCard: string;
+
+    constructor(
+        fullname: string,
+        email: string,
+        identityCard: string,
+    ) {
+        this.fullname = fullname;
+        this.email = email;
+        this.identityCard = identityCard;
+    }
+
+    getFullName(): string {
+        return this.fullname;
+    }
+
+    getEmail(): string {
+        return this.email;
+    }
+
+    getIdentityCard(): string {
+        return this.identityCard;
+    }
+}
