5d4198820a025463087fedbadb3362d07f3d4bea
Julian Martinez <julian.fmartinez@gmail.com>
2022-09-21 00:20:16 -0300

    add: handler for update passenger

---
+++ server/src/application/handlers/passengers/update.passenger.handler.ts
@@ -0,0 +1,17 @@
+import passengerRepository from '../../../infrastructure/repositories/passenger.repository';
+import { UpdatePassengerCommand } from '../../commands/update.passenger.command';
+
+class UpdatePassengerHandler {
+  async execute(command: UpdatePassengerCommand) {
+    const passenger = await passengerRepository.findOneByIdentityCard(command.getIdentityCard());
+
+    if (!passenger) {
+      throw new Error('Passenger not found');
+    }
+    passenger.changeEmail(command.getEmail());
+    passenger.changeFullname(command.getFullName());
+    await passengerRepository.save(passenger);
+  }
+}
+
+export default new UpdatePassengerHandler();
