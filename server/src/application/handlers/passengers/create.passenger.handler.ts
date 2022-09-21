872e203156256aef7d26c3a85776059a3944ddbc
Julian Martinez <julian.fmartinez@gmail.com>
2022-09-21 00:10:25 -0300

    add: service for create passenger

---
+++ server/src/application/handlers/passengers/create.passenger.handler.ts
@@ -0,0 +1,17 @@
+import { Passenger } from "../../../domain/entities/passenger.entity";
+import passengerRepository from "../../../infrastructure/repositories/passenger.repository";
+import { CreatePassengerCommand } from "../../commands/create.passenger.command";
+
+class CreatePassengerHandler {
+  async execute(command: CreatePassengerCommand) {
+    const passenger = Passenger.create(
+        command.getEmail(),
+        command.getFullName(),
+        command.getIdentityCard()
+    );
+
+    await passengerRepository.save(passenger);
+  }
+}
+
+export default new CreatePassengerHandler();
