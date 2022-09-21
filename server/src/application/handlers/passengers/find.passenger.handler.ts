9823158ec231d4e1221f82fbfee98418bb3ad009
Julian Martinez <julian.fmartinez@gmail.com>
2022-09-21 00:20:46 -0300

    add: handler for find passenger by identityCard

---
+++ server/src/application/handlers/passengers/find.passenger.handler.ts
@@ -0,0 +1,15 @@
+import passengerRepository from '../../../infrastructure/repositories/passenger.repository';
+import { UpdatePassengerCommand } from '../../commands/update.passenger.command';
+
+class FindPassengerHandler {
+  async execute(command: UpdatePassengerCommand) {
+    const passenger = await passengerRepository.findOneByIdentityCard(command.getIdentityCard());
+
+    if (!passenger) {
+      throw new Error('Passenger not found');
+    }
+    return passenger;
+  }
+}
+
+export default new FindPassengerHandler();
