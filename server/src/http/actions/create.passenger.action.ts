ab18d6e14abd6cb7a7bf03a3410c081485bacf50
Matias Laferla <matiaslaferla@gmail.com>
2022-09-26 09:27:06 -0300

    create-update passenger action

---
+++ server/src/http/actions/create.passenger.action.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+import { CreatePassengerCommand } from '../../../application/commands/create.passenger.command';
+import createPassengerHandler from '../../../application/handlers/passengers/create.passenger.handler';
+
+class CreatePassengerAction {
+  async run(req: Request, res: Response) {
+    const { fullName, email, identityCard } = req.body;
+
+    try {
+      const command = new CreatePassengerCommand(fullName, email, identityCard);
+      await createPassengerHandler.execute(command);
+
+      return res.status(201).json({ message: 'Passenger created successfully' });
+    } catch (error) {
+      const { message } = error as Error;
+      res.status(400).json({ message: message });
+    }
+  }
+}
+
+export default new CreatePassengerAction()
