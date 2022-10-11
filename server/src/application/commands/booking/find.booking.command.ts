7eaeab5e8bf5fca13a231b30939b2bf45d42a932
Romina Acevedo <romina@wakemedia.mx>
2022-10-11 12:57:23 -0300

    command booking

---
+++ server/src/application/commands/booking/find.booking.command.ts
@@ -0,0 +1,9 @@
+export class findBookingCommands{
+    private readonly passenger: string;
+    private readonly fromDate: Date;
+    
+    constructor(passenger: string, fromDate:Date){
+        this.passenger= passenger;
+        this.fromDate= fromDate;
+    }
+}
