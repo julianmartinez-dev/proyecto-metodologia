4ee7cd36c0f06aaf8b1dc12bca0af99ca248c8c1
Román Lazzarini <lazzariniroman@gmail.com>
2022-10-17 19:18:44 -0300

    Add route to update Booking

--- server/src/http/routes/booking.routes.ts
+++ server/src/http/routes/booking.routes.ts
@@ -1,17 +1,19 @@
 import { Application } from 'express';
 import CommonRoutes from './common.routes';
 import createBookingAction from '../actions/booking/create.booking.action';
+import updateBookingAction from '../actions/booking/update.booking.action';
 
 class BookingRoutes extends CommonRoutes {
-    constructor(app: Application) {
-      super(app, 'Booking');
-    }
-    setUpRoutes(): Application {
-  
-      this.app.post('/booking', createBookingAction.run);
-  
-      return this.app;
-    }
+  constructor(app: Application) {
+    super(app, 'Booking');
   }
-  
-  export default BookingRoutes;
+  setUpRoutes(): Application {
+    this.app.post('/booking', createBookingAction.run);
+
+    this.app.put('/booking/:id', updateBookingAction.run);
+
+    return this.app;
+  }
+}
+
+export default BookingRoutes;
