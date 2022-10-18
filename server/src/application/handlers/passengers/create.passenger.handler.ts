9421cb307b312a609d98e3877852467512684b28
Julian Martinez <julian.fmartinez@gmail.com>
2022-10-18 00:04:18 -0300

    add: validations on CreatePassengerHandler

--- server/src/application/handlers/passengers/create.passenger.handler.ts
+++ server/src/application/handlers/passengers/create.passenger.handler.ts
@@ -1,10 +1,25 @@
 import { Passenger } from '../../../domain/entities/passenger.entity';
+import { validateEmail, validateFullName, validateIdentityCard } from '../../../helpers';
 import passengerRepository from '../../../infrastructure/repositories/passenger.repository';
 import { CreatePassengerCommand } from '../../commands/passenger/create.passenger.command';
 
 class CreatePassengerHandler {
   async execute(command: CreatePassengerCommand) {
     const passenger = Passenger.create(command.getFullName(), command.getEmail(), command.getIdentityCard());
+
+    //Validate user name
+    if (!validateFullName(passenger.getFullName())) {
+      throw new Error('Invalid full name');
+    }
+    //Validate email from passenger
+    if (!validateEmail(passenger.getEmail())) {
+      throw new Error('Invalid email');
+    }
+
+    //Validate identity card
+    if (!validateIdentityCard(passenger.getIdentityCard())) {
+      throw new Error('Invalid identity card');
+    }
 
     await passengerRepository.save(passenger);
   }
