d8a96fc6b1e3320bcd6e6e8fc95dabdaa29f0c35
Julian Martinez <julian.fmartinez@gmail.com>
2022-10-18 00:04:44 -0300

    add: helpers folder

---
+++ server/src/helpers/index.ts
@@ -0,0 +1,14 @@
+export function validateEmail(email: string) : Boolean {
+    const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(email);
+}
+
+export function validateFullName(fullName: string) : Boolean {
+    const regex = /^[a-zA-Z ]{2,30}$/;
+    return regex.test(fullName);
+}
+
+export function validateIdentityCard(identityCard: string) : Boolean {
+    const regex = /^[0-9]{8}$/;
+    return regex.test(identityCard);
+}
