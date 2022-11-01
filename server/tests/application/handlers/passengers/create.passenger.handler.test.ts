646f6672aec321826e8c1f4f8b3d63e3c317856c
Julian Martinez <julian.fmartinez@gmail.com>
2022-10-31 21:10:14 -0300

    add: testing create passenger use cases

---
+++ server/tests/application/handlers/passengers/create.passenger.handler.test.ts
@@ -0,0 +1,41 @@
+import passengerRepository from '../../../../src/infrastructure/repositories/passenger.repository';
+import { Passenger } from '../../../../src/domain/entities/passenger.entity';
+
+import createPassengerHandler from '../../../../src/application/handlers/passengers/create.passenger.handler';
+import { CreatePassengerCommand } from '../../../../src/application/commands/passenger/create.passenger.command';
+
+describe('Create Passenger', () => {
+  it('should create a passenger', async () => {
+    const passengerMock = Passenger.create('Pepito perez', 'pepitoperez@yopmail.com', '12345678');
+    passengerRepository.save = jest.fn().mockResolvedValueOnce(passengerMock);
+    const result = await createPassengerHandler.execute(
+      new CreatePassengerCommand('Pepito perez', 'pepitoperez@yopmail.com', '12345678'),
+    );
+
+    expect(passengerRepository.save).toBeCalled();
+  });
+
+  it('should throw an error if identity card is invalid', async () => {
+    const command = new CreatePassengerCommand('Pepito perez', 'pepitoperez@yopmail.com', 'asd');
+    const result = await expect(createPassengerHandler.execute(command)).rejects.toStrictEqual(
+      new Error('Invalid identity card'),
+    );
+  });
+
+  it('should throw an error if email is invalid', async () => {
+    const command = new CreatePassengerCommand('Pepito perez', 'pepitoperez@yopmail', '11111111');
+    const result = await expect(createPassengerHandler.execute(command)).rejects.toStrictEqual(
+      new Error('Invalid email'),
+    );
+  });
+
+  it('should throw an error if passenger already exists', async () => {
+    const passengerMock = Passenger.create('Pepito perez', 'pepitoperez@yopmail.com', '12345678');
+    passengerRepository.findOneByIdentityCard = jest.fn().mockResolvedValueOnce(passengerMock);
+
+    const command = new CreatePassengerCommand('Pepito perez', 'pepitoperez@yopmail.com', '12345678');
+    const result = await expect(createPassengerHandler.execute(command)).rejects.toStrictEqual(
+      new Error('Passenger already exists'),
+    );
+  });
+});
