4d4e7443979aa26e398c8fd2bda42088d6dede12
Román Lazzarini <lazzariniroman@gmail.com>
2022-11-07 20:47:21 -0300

    joi relocated & add test

--- server/tests/application/handlers/accommodation/create.accommodation.handler.test.ts
+++ server/tests/application/handlers/accommodation/create.accommodation.handler.test.ts
@@ -2,13 +2,18 @@ import accommodationRepository from '../../../../src/infrastructure/repositories
 import { Accommodation } from '../../../../src/domain/entities/accommodation.entity';
 import CreateAccommodationHandler from '../../../../src/application/handlers/accommodation/create.accommodation.handler';
 import { CreateAccommodationCommand } from '../../../../src/application/commands/accommodation/create.accommodation.command';
+import createAccommodationHandler from '../../../../src/application/handlers/accommodation/create.accommodation.handler';
 
 describe('Create Accommodation', () => {
   it('should create an accommodation', async () => {
     const accommodationMock = Accommodation.create('Hotel', 50);
     accommodationRepository.save = jest.fn().mockResolvedValueOnce(accommodationMock);
-    const result = await CreateAccommodationHandler.execute(new CreateAccommodationCommand('10', 'Hotel', 50));
-    //En el command se solicita ID, ¿retirarlo?
+    const result = await CreateAccommodationHandler.execute(new CreateAccommodationCommand('Hotel', 50));
     expect(accommodationRepository.save).toBeCalled();
+  });
+
+  it('should throw an error if price per night is negative', async () => {
+    const command = new CreateAccommodationCommand('Hotel', -123);
+    await expect(createAccommodationHandler.execute(command)).rejects.toStrictEqual(new Error('Invalid PricePerNight'));
   });
 });
